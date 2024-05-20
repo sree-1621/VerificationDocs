3bb92ebbffe7a24a8f1c8bf1bb699f937fa8fc0b
sreechandana <Sreechandana.T@astiinfotech.com>
2024-05-20 12:41:08 +0530

    setup proxhy change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,24 +5,39 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = function (app) {
   console.log("Setting the Proxy Middleware....");
    
+  app.use(
+    '/proxyforapi',
+    createProxyMiddleware({
+      target: 'proxy IP',
+      changeOrigin: true,
+
+// -------- NOTES ----------------- 
+// For Spring Boot Backend
+      // Example : Target API is : https://www.xyz/abc/proxyforapi
+      //          Then Proxy IP will be '/proxyforapi'
+      //          If local ip is 'http://10.10.10.4658:8081'
+
+      //  So instead of calling the "http://10.10.10.4658:8081//proxyforapi"
+      //  it will call "https://www.xyz/abc/proxyforapi"
+
+      // This is basically for proxying the local ip with the server ip which we want to connect on
+      //  For target URL -> Use Valid IP;
+    }),
+  )
+
   let targetUrl="";
   const production = false;
-  const semiprod   = true;
-  const local      = false;
+  const semiprod   = false;
+  const local      = true;
 
-  //  Local Development
-  if (local === true && semiprod === false && production === false){
-    targetUrl = 'http://51.222.43.180:8901';
+  if(local == true && semiprod == false && production == false){
+    targetUrl = 'local server IP';
   }
-
-  //  Semi Production
-  else if(semiprod === true && local === false && production === false){
-    targetUrl = 'http://verificationsemiapi.astiinfotech.com';
+  else if(local == false && semiprod == true && production == false){
+    targetUrl = 'Semi production Server IP';
   }
-
-  // Production URL
-  else if(production === true && local === false && semiprod === false){
-    targetUrl = 'https://verificationapi.astiinfotech.com';
+  else if(local == false && semiprod == false && production == true){
+    targetUrl = 'Production Server IP';
   }
 
   // Proxy For PAN, License
@@ -87,5 +102,4 @@ module.exports = function (app) {
       changeOrigin: true,
     }),
   )
-
 }
